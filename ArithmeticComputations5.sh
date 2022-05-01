e9b07d70f2ecd307303a93099283f82c51ef487b
rohantare03 <rohatare123@gmail.com>
2022-05-01 10:18:40 +0530

    Fourth Arithmetic Computation

---
+++ ArithmeticComputations5.sh
@@ -0,0 +1,17 @@
+read -p "Enter First Number : " a
+read -p "Enter Second Number : " b
+read -p "Enter Third Number : " c
+
+echo "The Numbers are : $a $b $c"
+
+sum1=`echo $a $b $c | awk '{print $1+$2*$3}'`;
+echo "$a+$b*$c = $sum1"
+
+sum2=`echo $a $b $c | awk '{print $1*$2+$3}'`;
+echo "$a*$b+$c = $sum2"
+
+sum3=`echo $a $b $c | awk '{print $3+$1/$2}'`;
+echo "$c+$a/$b = $sum3"
+
+sum4=`echo $a $b $c | awk '{print $1%$2+$3}'`;
+echo "$a%$b+$c = $sum4"
