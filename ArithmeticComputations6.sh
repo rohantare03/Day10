7b885eaa35957bc45765136618ff2f6eff77d960
rohantare03 <rohatare123@gmail.com>
2022-05-01 10:40:44 +0530

    Storing the Computation results in Dictionary

---
+++ ArithmeticComputations6.sh
@@ -0,0 +1,21 @@
+read -p "Enter First Number : " a
+read -p "Enter Second Number : " b
+read -p "Enter Third Number : " c
+
+echo "The Numbers are : $a $b $c"
+
+sum1=`echo $a $b $c | awk '{print $1+$2*$3}'`;
+echo "$a+$b*$c = $sum1"
+
+sum2=`echo $a $b $c | awk '{print $1*$2+$3}'`;
+echo "$a*$b+$c = $sum2"
+
+sum3=`echo $a $b $c | awk '{print $3+$1/$2}'`;
+echo "$c+$a/$b = $sum3"
+
+sum4=`echo $a $b $c | awk '{print $1%$2+$3}'`;
+echo "$a%$b+$c = $sum4"
+
+declare -A results=( [sum1]="$sum1" [sum2]="$sum2" [sum3]="$sum3" [sum4]="$sum4" )
+echo "The Computation results in Dictionary : ${results[@]}"
+
