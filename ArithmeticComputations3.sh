23e4d9da935c3e985b3511d9d810ab24bca06fab
rohantare03 <rohatare123@gmail.com>
2022-05-01 10:04:04 +0530

    Second Arithmetic Computation

---
+++ ArithmeticComputations3.sh
@@ -0,0 +1,11 @@
+read -p "Enter First Number : " a
+read -p "Enter Second Number : " b
+read -p "Enter Third Number : " c
+
+echo "The Numbers are : $a $b $c"
+
+sum1=`echo $a $b $c | awk '{print $1+$2*$3}'`;
+echo "$a+$b*$c = $sum1"
+
+sum2=`echo $a $b $c | awk '{print $1*$2+$3}'`;
+echo "$a*$b+$c = $sum2"
